4308850f8e06186ad145c141eb94336ad6a0b5b9
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-07-21 08:41:34 +0700

    drlate With (In) R Software

--- drlate_/drlate_.R
+++ drlate_/drlate_.R
@@ -1,15 +1,14 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) Software
+# Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) R Software
 install.packages("drlate")
-
 library("drlate")
-# Estimation Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) Software
+# Estimation Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) R Software
 drlate_ = read.csv("https://raw.githubusercontent.com/timbulwidodostp/drlate_/main/drlate_/drlate_.csv",sep = ";")
 drlate <- drlate(lwage ~ age + educ, nvstat ~ age + educ, rsncode ~ age + educ, data = drlate_)
 summary(drlate)
-# Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) Software
+# Doubly robust estimation of the local average treatment effect (LATE) and the local average treatment effect on the treated (LATT) Use drlate With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
